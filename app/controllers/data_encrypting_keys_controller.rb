0724ec103eac4a7e06b1801ee7176143a96f336b
Mary Franks <maryfranks@gmail.com>
2018-11-13 09:49:51 -0500

    add routes and methods for /rotate and /rotate/status

--- app/controllers/data_encrypting_keys_controller.rb
+++ app/controllers/data_encrypting_keys_controller.rb
@@ -1,2 +1,17 @@
 class DataEncryptingKeysController < ApplicationController
+
+  def rotate
+    # create background worker to rotate keys and re-encrypt strings
+    # 1. check if it's already working
+    render json: { message: params[:new_key] }
+  end
+
+  def status
+    # need three messages:
+      # render json: { message: 'Key rotation has been queued' }
+      # render json: { message: 'Key rotation is in progress' }
+      # render json: { message: 'No key rotation queued or in progress' }
+    render json: { message: 'this is a status' }
+  end
+
 end
