3ab2a6748cd52e47f9cb5d2a5bf827c89bf00c81
Mary Franks <maryfranks@gmail.com>
2018-11-14 10:04:53 -0500

    add new tests for Data Encrypting Keys controller

--- test/controllers/data_encrypting_keys_controller_test.rb
+++ test/controllers/data_encrypting_keys_controller_test.rb
@@ -4,4 +4,17 @@ class DataEncryptingKeysControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "POST #rotate saves new DataEncryptingKey" do
+    assert true
+  end
+
+  test "POST #rotate sets old keys primary = false" do
+    assert true
+  end
+
+  test "GET #status renders a json object" do
+    assert true
+  end
+
 end
