8a0bca27ae9c90eb60c1bdc6c7f0b025d4207fbc
Mary Franks <maryfranks@gmail.com>
2018-11-15 15:31:26 -0500

    send encrypted strings and key to background worker

--- app/controllers/data_encrypting_keys_controller.rb
+++ app/controllers/data_encrypting_keys_controller.rb
@@ -7,6 +7,8 @@ class DataEncryptingKeysController < ApplicationController
     @data_encrypting_key = DataEncryptingKey.new(key: params[:data_encrypting_key], primary: true)
     if @data_encrypting_key.save
       @old_encrypting_key.remove_primary
+      @encrypted_strings = EncryptedString.all.map { |string| string.encrypted_value }
+      ReencryptStringsWorker.perform_async(@encrypted_strings, @old_encrypting_key.encrypted_key)
       render json: { key: @data_encrypting_key.key, primary: @data_encrypting_key.primary, old_key: @old_encrypting_key.primary, count: DataEncryptingKey.all.count }
     else
       render json: { message: @data_encrypting_key.errors.full_messages.to_sentence}
--- app/workers/reencrypt_strings_worker.rb
+++ app/workers/reencrypt_strings_worker.rb
@@ -1,7 +1,13 @@
 class ReencryptStringsWorker
   include Sidekiq::Worker
 
-  def perform(*args)
-    # Do something
+  def perform(encrypted_strings, key)
+    encrypted_strings.each do | string |
+      puts string
+      puts key
+      # 1. decrypt the string - will need old key
+      # 2. reencrypt the string
+      # 3. give the strings back to the controller
+    end
   end
 end
